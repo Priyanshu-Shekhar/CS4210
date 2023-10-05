3025062faa1769d9a7b3d9c7c4c0c90b69882032
Priyanshu Shekhar <114723518+Priyanshu-Shekhar@users.noreply.github.com>
2023-10-05 15:16:08 -0700

    Assignment 2 - 3 pytho files and 7 csv files

---
+++ Assignment 2/knn.py
@@ -0,0 +1,89 @@
+#-------------------------------------------------------------------------
+# AUTHOR: Priyanshu Shekhar
+# FILENAME: knn.py
+# SPECIFICATION: the code 
+# FOR: CS 4210- Assignment #2
+# TIME SPENT: 4 hours
+#-----------------------------------------------------------*/
+
+#IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard vectors and arrays
+
+#importing some Python libraries
+from sklearn.neighbors import KNeighborsClassifier
+import csv, copy
+
+#function to convert 2D array value to float
+def value_to_float_2D(array2D):
+    for i in range(len(array2D)):
+        for j in range(len(array2D[i])):
+            array2D[i][j] = float(array2D[i][j])
+    return array2D
+
+def value_to_float_1D(array):
+    for i in range(len(array)):
+        array[i] = float(array[i])
+    return array
+
+db = []
+num_of_incorrect_prediction = 0
+
+#reading the data in a csv file
+with open('binary_points.csv', 'r') as csvfile:
+  reader = csv.reader(csvfile)
+  for i, row in enumerate(reader):
+      if i > 0:
+         db.append (row)
+
+
+
+#loop your data to allow each instance to be your test set
+for data in db:
+    #add the training features to the 2D array X removing the instance that will be used for testing in this iteration. For instance, X = [[1, 3], [2, 1,], ...]]. 
+    training_db = copy.deepcopy(db)
+    training_db.remove(data)
+    X = training_db
+    training_class_set = []
+
+    for i in range(len(X)):
+        training_class_set.append(X[i][2])
+        del X[i][2]
+    
+    #convert 2D str array to float array
+    X = value_to_float_2D(X)
+    
+    #transform the original training classes to numbers and add to the vector Y removing the instance that will be used for testing in this iteration. For instance, Y = [1, 2, ,...]. 
+    #1 = positive, 2 = negative
+    for i in range(len(training_class_set)):
+        if training_class_set[i] == "+":
+            training_class_set[i] = 1
+        elif training_class_set[i] == "-":
+            training_class_set[i] = 2
+
+    Y = training_class_set
+
+    #convert 1D int array to float array
+    Y = value_to_float_1D(Y)    
+
+    testSample = data[0:2]
+    testSample = value_to_float_1D(testSample)
+    test_class = data[2]
+
+    if test_class == "+":
+        test_class = 1.0
+    elif test_class == "-":
+        test_class = 2.0
+    
+    #fitting the knn to the data
+    clf = KNeighborsClassifier(n_neighbors=1, p=2)
+    clf = clf.fit(X, Y)
+
+    #get prediction with testing data
+    class_predicted = clf.predict([testSample])[0]
+
+    #compare the prediction with the true label of the test instance to start calculating the error rate.
+    if class_predicted != test_class:
+        num_of_incorrect_prediction += 1
+
+#print the error rate
+error_rate = num_of_incorrect_prediction / len(db)
+print("The error rate for 1NN algorithm:",error_rate)
---
+++ Assignment 2/naive_bayes.py
@@ -0,0 +1,122 @@
+#-------------------------------------------------------------------------
+# AUTHOR: Priyanshu Shekhar
+# FILENAME: naive_bayes.py
+# SPECIFICATION: Classify testing data based on training dataset
+# FOR: CS 4210- Assignment #2
+# TIME SPENT: 5-6 hours
+#-----------------------------------------------------------*/
+
+# IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard
+# dictionaries, lists, and arrays
+
+#importing some Python libraries
+from sklearn.naive_bayes import GaussianNB
+import csv,copy
+
+def transform_feature_to_num(dataset):
+    training_class_set = []
+    day_class_set = []
+    for i in range(len(dataset)):
+        for j in range(1,len(dataset[i])-1):
+            if j == 1:
+                match dataset[i][j]:
+                    case "Sunny":
+                        dataset[i][j] = 1
+                    case "Overcast":
+                        dataset[i][j] = 2
+                    case "Rain":
+                        dataset[i][j] = 3
+            elif j == 2:
+                match dataset[i][j]:
+                    case "Hot":
+                        dataset[i][j] = 1
+                    case "Mild":
+                        dataset[i][j] = 2
+                    case "Cool":
+                        dataset[i][j] = 3
+            elif j == 3:
+                match dataset[i][j]:
+                    case "High":
+                        dataset[i][j] = 1
+                    case "Normal":
+                        dataset[i][j] = 2
+            elif j == 4:
+                match dataset[i][j]:
+                    case "Weak":
+                        dataset[i][j] = 1
+                    case "Strong":
+                        dataset[i][j] = 2
+
+    for i in range(len(dataset)):
+        training_class_set.append(dataset[i][5])
+        del dataset[i][5]
+        day_class_set.append(dataset[i][0])
+        del dataset[i][0]
+
+    for i in range(len(training_class_set)):
+        if training_class_set[i] == "Yes":
+            training_class_set[i] = 1
+        elif training_class_set[i] == "No":
+            training_class_set[i] = 2    
+    return dataset,training_class_set,day_class_set
+
+# These two functions are used to make a nice looking output
+def pad_word(word, length):
+    word = str(word)
+    if len(word) < length:
+        padded_word = word + ' ' * (length - len(word))
+        return padded_word
+    else:
+        return word
+    
+def print_row(c1,c2,c3,c4,c5,c6,c7):
+    print(f'{pad_word(c1, 3)}   {pad_word(c2, 8)}   {pad_word(c3, 11)}   {pad_word(c4, 8)}   {pad_word(c5, 6)}   {pad_word(c6, 10)}   {pad_word(c7, 10)}')
+
+#reading the training data in a csv file
+db = []
+
+with open('weather_training.csv', 'r') as csvfile:
+  reader = csv.reader(csvfile)
+  for i, row in enumerate(reader):
+      if i > 0: #skipping the header
+         db.append (row)
+
+#transform the original training features to numbers and add them to the 4D array X.
+#For instance Sunny = 1, Overcast = 2, Rain = 3, so X = [[3, 1, 1, 2], [1, 3, 2, 2], ...]]
+
+#get the training feature and training class
+X,Y,day_training = transform_feature_to_num(db)
+
+#fitting the naive bayes to the data
+clf = GaussianNB()
+clf.fit(X, Y)
+
+#reading the test data in a csv file
+test_db = []
+with open('weather_test.csv', 'r') as csvfile:
+  reader = csv.reader(csvfile)
+  for i, row in enumerate(reader):
+      if i > 0: #skipping the header
+         test_db.append (row)
+
+newdb = copy.deepcopy(test_db)
+
+#printing the header os the solution
+data_testX,data_testY,day_test = transform_feature_to_num(test_db)
+
+#print headers
+print_row("Day","Outlook","Temperature","Humidity","Wind","PlayTennis","Confidence")
+
+count = 0
+for instance in data_testX:
+    probability_predicted = clf.predict_proba([instance])[0]
+    if probability_predicted[0] >= 0.75:
+        print_row(newdb[count][0],newdb[count][1],newdb[count][2],newdb[count][3],newdb[count][4],"Yes","%.2f" % probability_predicted[0])
+        count+=1
+    elif probability_predicted[1] >= 0.75:
+        print_row(newdb[count][0],newdb[count][1],newdb[count][2],newdb[count][3],newdb[count][4],"No","%.2f" % probability_predicted[1])
+        count+=1
+    else:
+        count+=1
+        
+    
